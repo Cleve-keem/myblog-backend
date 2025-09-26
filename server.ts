3730ad815360f12bfa7b4d704317b4aabc45936b
Cleve-keem <belloharyourmidey@gmail.com>
2025-09-26 06:55:10 -0500

    added my netlify  origin

--- server.ts
+++ server.ts
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://hackhimblog.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
