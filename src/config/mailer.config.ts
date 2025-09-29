6c56b5e9e8999278ae36a31c24d57428d693bff6
Cleve-keem <belloharyourmidey@gmail.com>
2025-09-29 11:02:13 -0500

    updated the tls to reject unauthorized

--- src/config/mailer.config.ts
+++ src/config/mailer.config.ts
@@ -6,13 +6,13 @@ dotenv.config();
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
   tls: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: process.env.NODE_ENV !== "production",
   },
 });
 
