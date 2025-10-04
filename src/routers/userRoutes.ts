5839d23a2ba0e38543259ad6497bb722d6358407
Cleve-keem <belloharyourmidey@gmail.com>
2025-10-04 08:01:48 -0500

    updated user routes from auth to account

--- src/routers/userRoutes.ts
+++ src/routers/userRoutes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { UserController } from "../controller/UserController";
 
 const router = Router();
-router.post("/auth/sign-up", UserController.registerUser);
-router.post("/auth/login", UserController.loginUser);
+router.post("/account/sign-up", UserController.registerUser);
+router.post("/account/login", UserController.loginUser);
 
 export default router;
