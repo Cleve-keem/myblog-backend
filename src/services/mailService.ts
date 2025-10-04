fd095a44499f416901f94b758bbe8a6847a48f0e
Cleve-keem <belloharyourmidey@gmail.com>
2025-10-04 07:57:52 -0500

    updated verify url link

--- src/services/mailService.ts
+++ src/services/mailService.ts
@@ -15,7 +15,7 @@ export default class MailService {
         <h1 style="margin: 0; padding: 10px 0;">Hello, ${data.lastname} 👋!</h1>
         
         <p>Thanks for signing up with Hackhim Platform! To complete your registration and activate your account, please verify your email by clicking the link below 🔗</p>
-        <a href="https://hackhimblog.netlify.app/auth/account/verify/${token}" style="background: #000; color:#fff; padding: 10px 20px; text-decoration: none; border-radius: 6px; margin: 10px 0;">Verify account</a>
+        <a href="https://hackhimblog.netlify.app/account/verify/${token}" style="background: #000; color:#fff; padding: 10px 20px; text-decoration: none; border-radius: 6px; margin: 10px 0;">Verify account</a>
 
         <p>if you didn't request this, feel free to ignore the message. But if you're ready to get started, just click the link and you're all set!</p>
 
